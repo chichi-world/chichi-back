92c007dfe85dfcfaa13f222a8dfb7ce9f0f74f1b
dlwtja <dlwtja1@naver.com>
2024-10-12 19:18:50 +0900

    feat: add isSocialLogin field to user model and update logic for social login users

--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,7 @@ const User = sequelize.define(
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true, // 소셜 로그인 시 비밀번호는 없을 수 있음
     },
     signupDate: {
       type: DataTypes.DATE,
@@ -32,6 +32,11 @@ const User = sequelize.define(
       type: DataTypes.DATE,
       allowNull: true, // 마지막 접속 날짜
     },
+    isSocialLogin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false, // 기본값은 소셜 로그인 아님
+    },
   },
   {
     timestamps: true, // createdAt 및 updatedAt 필드를 자동으로 생성
@@ -41,12 +46,18 @@ const User = sequelize.define(
 
 // 비밀번호 암호화 (유저 생성 전)
 User.beforeCreate(async (user) => {
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  if (!user.isSocialLogin) {
+    // 소셜 로그인이 아닌 경우에만 비밀번호 해시 처리
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
 });
 
 // 비밀번호 비교 메서드
 User.prototype.comparePassword = async function (inputPassword) {
+  if (this.isSocialLogin) {
+    throw new Error("Social login users do not have a password.");
+  }
   return await bcrypt.compare(inputPassword, this.password);
 };
 
